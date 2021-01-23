699102c8ea7e5393ed4fd2ed4a6c5738cdf46cf5
cosmin <cosmin.gedget@gmail.com>
2021-01-23 15:35:49 +0200

    Add Autoturism class

---
+++ src/Autoturism.java
@@ -0,0 +1,10 @@
+/**
+ * Clasa ce descrie un vehicul de tip Autoturism
+ */
+public class Autoturism extends Vehicul {
+    Autoturism() {
+        this.setTip("Autovehicul");
+        this.setGabarit(2);
+        this.setCost(4);
+    }
+}
